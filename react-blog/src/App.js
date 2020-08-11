4d3b10f86f5761c54927488d563dd92feb259c23
Daniel Draskov <ddraskovv@gmail.com>
2020-08-11 17:27:43 +0300

    SignIn/SignUp components

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import PostDetails from './components/posts/PostDetails'
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component{
   
@@ -15,6 +16,7 @@ class App extends Component{
         <Route exact path='/' component={Dashboard} />
         <Route path='/post/:id' component={PostDetails} />
         <Route path='/signin' component={SignIn} />
+        <Route path='/signup' component={SignUp} />
       </Switch>
     </div>
     </BrowserRouter>
