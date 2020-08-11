038878cfa675330713f9923bcf2013b12c2a1227
Daniel Draskov <ddraskovv@gmail.com>
2020-08-11 08:10:08 +0300

    added navbar

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar'
 
-function App() {
-  return (
+class App extends Component{
+  
+  render(){return (
+    <BrowserRouter>
     <div className="App">
-      <h1>University</h1>
+      <Navbar />
     </div>
+    </BrowserRouter>
   );
+  }
 }
 
 export default App;
