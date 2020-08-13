c0e5d5783ef2774e1825e26428c5e1689882eb0a
Daniel Draskov <ddraskovv@gmail.com>
2020-08-13 06:55:04 +0300

    links when loggen in/out

--- react-blog/src/components/layout/Navbar.js
+++ react-blog/src/components/layout/Navbar.js
@@ -4,13 +4,16 @@ import SignedInLinks from './SignedinLinks';
 import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">ReactBlog</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                { links }
             </div>
         </nav>
     )
@@ -18,7 +21,7 @@ const Navbar = () => {
 
 const mapStateToProps = (state) => {
     return {
-
+        auth: state.firebase.auth
     }
 }
 
